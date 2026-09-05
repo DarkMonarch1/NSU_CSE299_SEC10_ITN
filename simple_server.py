aca4d7455504bf636bf02c8d83c5630ef1428520
darkmonarch1 <www.ratulboss@gmail.com>
2026-09-05 21:45:41 +0600

    roll back v2

---
+++ simple_server.py
@@ -0,0 +1,22 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(b'{"status":"ok","message":"Simple server working"}')
+    
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        super().end_headers()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    print(f"Starting simple server on port {port}")
+    server.serve_forever()
